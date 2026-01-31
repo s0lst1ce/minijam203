4552ee3ec31d5d43a5a89c4289bc2dec2b0a2442
s0lst1ce <cadmium-beaver@pm.me>
2026-01-31 15:52:06 +0100

    feat: impl gravity

--- dog_character.gd
+++ dog_character.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 
 @onready var screensize = get_viewport_rect().size
+@onready var gravity = float(ProjectSettings.get_setting("physics/2d/default_gravity"))
 
-const SPEED: int = 400
+
+const SPEED: float = 400
 
 var current_mode: int = 0
 
@@ -13,8 +15,16 @@ func refresh_velocity_from_input():
 	var input_direction: Vector2 = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * SPEED
 
+
 func _physics_process(delta: float) -> void:
 	refresh_velocity_from_input()
+	#print(velocity)
+	
+	if not is_on_floor():
+		#print(velocity)
+		velocity.y += gravity * delta
+		#print(velocity)
+	
 	move_and_slide()
 	
 	#making sure we stay in-bounds
