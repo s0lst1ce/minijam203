8ba02f0221f3aaac873b80373670b9a0ad4d6042
s0lst1ce <cadmium-beaver@pm.me>
2026-01-31 17:57:40 +0100

    feat: add moving platforms

---
+++ moving_platform.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+@export var offset = Vector2(300, 0)
+@export var duration = 5.0
+@onready var initial_position = position
+
+func _ready() -> void:
+	start_tween()
+	
+func start_tween():
+	var tween =get_tree().create_tween().set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
+	tween.set_loops().set_parallel(false)
+	tween.tween_property($AnimatableBody2D, "position", offset, duration/2)
+	tween.tween_property($AnimatableBody2D, "position", initial_position, duration/2)
+	
+	
