b72eef3eead4fbd97f9f81fa792ecc8050c8264e
Hugo (aka Xelodrin) <HugoFerrantPro@gmail.com>
2026-01-31 15:24:36 +0100

    chore: use "_" for unused mandatory params

--- dog_character.gd
+++ dog_character.gd
@@ -13,14 +13,14 @@ func refresh_velocity_from_input():
 	var input_direction: Vector2 = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * SPEED
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	refresh_velocity_from_input()
 	move_and_slide()
 	
 	#making sure we stay in-bounds
 	position = position.clamp(Vector2(0, 0), screensize)
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("cycle_mode"):
 		set_collision_layer_value(current_mode + 5, false)
 		set_collision_mask_value(current_mode + 5, false)
