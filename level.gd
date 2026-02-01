713542aa346636959ba0e89b5fdde15dfb9bc9e7
s0lst1ce <cadmium-beaver@pm.me>
2026-02-01 12:37:32 +0100

    fix: cyclic dependency

--- level.gd
+++ level.gd
@@ -1,13 +1,12 @@
 extends Node2D
 
-@export var next_scene: PackedScene
+@export var next_scene_path: String
 
 func level_complete():
-	get_tree().change_scene_to_packed(next_scene)
+	get_tree().change_scene_to_file(next_scene_path)
 
 func _ready() -> void:
 	$DogCharacter.connect("level_finish_reached", _on_level_finish_reached)
 
 func _on_level_finish_reached() -> void:
-	print("ha", next_scene)
 	level_complete()
