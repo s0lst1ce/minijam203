1ee5fc1a31a4c993e897010a8724f8bc6e9134a6
s0lst1ce <cadmium-beaver@pm.me>
2026-02-06 12:17:32 +0100

    fix: stuck level when exiting moving platform levels

--- moving_platform.gd
+++ moving_platform.gd
@@ -11,6 +11,6 @@ func start_tween():
 	var tween = get_tree().create_tween().set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
 	tween.set_loops().set_parallel(false)
 	tween.tween_property($AnimatableBody2D, "position", offset, duration/2)
-	tween.tween_property($AnimatableBody2D/Sprite2D, "flip_h", true, 0)
+	tween.tween_property($AnimatableBody2D/Sprite2D, "flip_h", true, 0.1)
 	tween.tween_property($AnimatableBody2D, "position", Vector2.ZERO, duration/2)
-	tween.tween_property($AnimatableBody2D/Sprite2D, "flip_h", false, 0)
+	tween.tween_property($AnimatableBody2D/Sprite2D, "flip_h", false, 0.1)
